d1771d8699bb778f849d00faeac4ba9b68e914fa
Kemono995 <kemono995@gmail.com>
2018-07-25 21:16:49 +0200

    Pocetak Entity

---
+++ Baza/Baza/Baza/BankaContext.cs
@@ -0,0 +1,20 @@
+﻿using CrveniKrst;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza.Baza
+{
+    public class BankaContext : DbContext
+    {
+
+        public DbSet<BankaKrvi> BankeKrvi { get; set; }
+        public DbSet<Donor> Donori { get; set; }
+        public DbSet<Pacijent> Pacijenti { get; set; }
+        public DbSet<JedinicaTransfuzije> JediniceTransfuzije { get; set; }
+
+    }
+}
