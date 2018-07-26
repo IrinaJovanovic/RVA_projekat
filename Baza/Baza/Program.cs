722065f5327203941e2cba3b1de8573afb475bda
IrinaJovanovic <39349895+IrinaJovanovic@users.noreply.github.com>
2018-07-27 00:09:40 +0200

    nesto
    nesto

---
+++ Baza/Baza/Program.cs
@@ -0,0 +1,90 @@
+﻿using Baza.Baza;
+<<<<<<< HEAD
+using Baza.Servis;
+using Common.DTO;
+=======
+>>>>>>> 3391f6091eefa9c4d3d6dfa8d784624df25b3975
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza
+{
+    class Program
+    {
+        private static ServerServis serverServis = new ServerServis();
+        static void Main(string[] args)
+        {
+<<<<<<< HEAD
+            insertData();
+            test2();
+            test1();
+            test2();
+           // insertData();
+
+            
+            Console.Read();
+            serverServis.ObrisiBazuJerJeEntityRetardiran();
+        }
+
+        private static void test2()
+        {
+            foreach (var item in serverServis.ReadAllDonor())
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        private static void test1()
+        {
+               
+            char[] krvnaGrupa = { 'A' };
+
+            DonorDTO donorDTO = new DonorDTO();
+            donorDTO.Id = 1;
+            donorDTO.KrvnaGrupa = krvnaGrupa;
+            serverServis.UpdateDonor(donorDTO);
+            
+
+            return;
+        }
+
+        private static void insertData()
+        {
+            
+            char[] krvnaGrupa = { 'A' };
+
+            DonorDTO donorDTO = new DonorDTO();
+            donorDTO.Ime = "Ime";
+            donorDTO.KrvnaGrupa = krvnaGrupa;
+            donorDTO.Prezime = "prezime";
+            DonorDTO retVal = serverServis.CreateDonor(donorDTO);
+
+            
+=======
+
+
+
+            using (var context = new BankaContext())
+            {
+                char[] temp ={ 'B', '-' };
+                context.Pacijenti.Add(new CrveniKrst.Pacijent("ime", temp, "prezime"));
+                context.SaveChanges();
+            }
+
+            using (var context = new BankaContext())
+            {
+
+                foreach (var item in context.Pacijenti)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+                
+            }
+            Console.Read();
+>>>>>>> 3391f6091eefa9c4d3d6dfa8d784624df25b3975
+        }
+    }
+}
---
+++ Baza/Baza/Servis/ServerServis.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Baza.Baza;
+using Common.DTO;
+
+
+namespace Baza.Servis
+{
+    public class ServerServis : IDonorServis
+    {
+
+
+        
+
+        public DonorDTO CreateDonor(DonorDTO donorDTO)
+        {
+            Donor retVal = null;
+            using (var context = new BankaContext()) //citava baza mi je u prom context sada 
+            {
+                retVal = new Donor(donorDTO);
+                context.Donori.Add(retVal);
+                context.SaveChanges();
+                                
+
+
+            }
+            if(retVal==null)
+            {
+                return null;
+            }
+            return new DonorDTO(retVal.Id, retVal.Ime, retVal.KrvnaGrupa, retVal.Prezime);
+        }
+
+        public DonorDTO DeleteDonor(DonorDTO donorDTO)
+        {
+            // throw new NotImplementedException();
+            Donor retVal = null;
+            using (var context = new BankaContext())
+            {
+                foreach (var item in context.Donori)
+                {
+                    if(item.Id.Equals(donorDTO.Id))
+                    {
+                        retVal = item;
+                        break;
+                    }
+                }
+                if(retVal==null)
+                {
+                    return null;
+                }
+
+
+         
+                context.Donori.Remove(retVal);
+                context.SaveChanges();
+            }
+            
+                 return new DonorDTO(retVal.Id, retVal.Ime, retVal.KrvnaGrupa, retVal.Prezime);
+        }
+
+        public List<DonorDTO> ReadAllDonor()
+        {
+            // throw new NotImplementedException();
+            List<DonorDTO> povratna = new List<DonorDTO>();
+            using (var context = new BankaContext())
+            {
+                foreach (var item in context.Donori)
+                {
+                    povratna.Add(new DonorDTO(item.Id, item.Ime, item.KrvnaGrupa, item.Prezime));
+                }
+            }
+            return povratna;
+
+           
+        }
+
+        public List<DonorDTO> ReadByFilterDonor(DonorDTO donorDTO)
+        {
+            // throw new NotImplementedException();
+            List<DonorDTO> povratna = new List<DonorDTO>();
+            using (var context = new BankaContext())
+            {
+                foreach (var item in context.Donori)
+                {
+                    if (item.MatchesFilter(donorDTO))
+                    {
+                        povratna.Add(new DonorDTO(item.Id, item.Ime, item.KrvnaGrupa, item.Prezime));
+                    }
+                }
+            }
+            return povratna;
+
+        }
+
+        public bool UpdateDonor(DonorDTO donorDTO)
+        {
+            using (var context = new BankaContext())
+            {
+                Donor noviDonor = new Donor(donorDTO);
+                Donor stariDonor =null;
+                foreach (var item in context.Donori)
+                {
+                    if (item.Id.Equals(donorDTO.Id))
+                    {
+                        stariDonor = item;
+                        break;
+                    }
+                }
+                if (stariDonor != null)
+                {
+                    stariDonor = noviDonor;
+                }
+
+                
+
+                
+                context.SaveChanges();
+            }
+            return true;
+        }
+
+        public void ObrisiBazuJerJeEntityRetardiran()
+        {
+            using (var context = new BankaContext())
+            {
+                context.Database.Delete();
+            }
+                
+        }
+    }
+}
